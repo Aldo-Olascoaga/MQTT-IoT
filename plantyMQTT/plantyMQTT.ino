17d44ef2517d1c67287077fbb64079daeb0db84b
Pato1302 <patalvher@hotmail.com>
2023-05-11 22:11:15 -0600

    Setup, loop and WIFI

--- plantyMQTT/plantyMQTT.ino
+++ plantyMQTT/plantyMQTT.ino
@@ -1,40 +1,52 @@
-#include "DHT.h"
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-//#include <ArduinoJson.h>
-#include <PubSubClient.h>
-
-
-//========= MQTT CONFIG =========
-const char* mqtt_server = "54.173.105.103"; //IP of instance AWS
-const int mqtt_port = 1883;
-const char* mqtt_user = "";
-const char* mqtt_pwd = "";
-
-
-// ============ GLOBAL =============
-WiFiClient ESP_client;
-PubSubClient (ESP_client);
-
-//DHT dht(2, DHT11);  // DHT read from digital pin 4
-#define DHTTYPE DHT11 // DHT 11
-int DHTPin = 2; // Pin D4 en el MCU
-DHT dht(DHTPin, DHTTYPE);
-
-WiFiServer server(80);
-
-int d4 = 14;  //Fotorresistencia  
-
-
-
-
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  sensors_setup();
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  if (!client.connected()) {
+    reconnect();
+  }
 
+  if (client.connected()){
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    String data = "Humedad: " + String(h) + "% ";
+    data = data + "Temperatura: " + String(t) + "°C ";
+    
+    Serial.println(data);
+    data.toCharArray(msg, 100);
+
+    client.publish(root_topic_publish, msg);
+  }
+  client.loop();
+}
+
+
+
+
+//========= Conexion WIFI =========
+void setup_wifi(){
+  delay(10);
+  // Nos conectamos a nuestra red Wifi
+  Serial.println();
+  Serial.print("Conectando a ssid: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Conectado a red WiFi!");
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
 }
