7ed838f711806a65a5cd3a8f255bf8a84e45ba1f
= <=>
2023-05-11 22:38:14 -0600

    New functions and comments added

--- plantyMQTT/plantyMQTT.ino
+++ plantyMQTT/plantyMQTT.ino
@@ -99,3 +99,54 @@ void setup_wifi(){
   Serial.println("Dirección IP: ");
   Serial.println(WiFi.localIP());
 }
+
+
+//========= MQTT CONNECTION =========    
+// In case connection fails...
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Intentando conexión Mqtt...");
+    // We create a client
+    String clientId = "WIX_Team";
+    clientId += String(random(0xffff), HEX);
+    // Let's try to connect...
+    if (client.connect(clientId.c_str(),mqtt_user,mqtt_pass)) {
+      Serial.println("Conectado!");
+      // Let's suscribe
+      if(client.subscribe(root_topic_subscribe)){
+        Serial.println("Suscripcion ok");
+      }else{
+        Serial.println("Fallo Suscripción");
+      }
+    } else {
+      Serial.print("falló :( con error -> ");
+      Serial.print(client.state());
+      Serial.println(" Intentamos de nuevo en 5 segundos");
+      delay(5000);
+    }
+  }
+}
+
+
+//========= CALLBACK =========
+// Callback function for the message
+void callback(char* topic, byte* payload, unsigned int length){
+  String incoming = "";
+  Serial.print("Mensaje recibido desde -> ");
+  Serial.print(topic);
+  Serial.println("");
+  for (int i = 0; i < length; i++) {
+    incoming += (char)payload[i];
+  }
+  incoming.trim();
+  Serial.println("Mensaje -> " + incoming);
+
+}
+
+
+//========= SENSORS SETUP =========
+
+// The purpose of this function is to start the DHT11 sensor so it works smoothly
+void sensors_setup(){
+  dht.begin();
+}
