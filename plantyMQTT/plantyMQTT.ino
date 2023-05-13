40785b7806eef587ca092bf71ac82d008b064238
Aldo Olascoaga Olmedo <69182604+Aldo-Olascoaga@users.noreply.github.com>
2023-05-12 20:20:52 -0600

    New meaningful comments added

--- plantyMQTT/plantyMQTT.ino
+++ plantyMQTT/plantyMQTT.ino
@@ -9,58 +9,77 @@
 #include <ESP8266HTTPClient.h>
 #include <PubSubClient.h>
 
-
-
 //========== MQTT CONFIG ==========
+
+// MQTT server IP address
 const char *mqtt_server = "IP ADRESS"; //IP of instance in AWS
 const int mqtt_port = 1883; // Port open for MQTT
 const char *mqtt_user = ""; // In case of Log In
 const char *mqtt_pass = ""; // In case of Log In
+
+// MQTT topics
 const char *root_topic_subscribe = "/input"; // Topic to pub -> Send Info
 const char *root_topic_publish = "/output"; // Topic to sub  -> Receive Info
 
-
-
 //========== WIFI CONFIG ==========
+
+// WiFi network credentials
 char* ssid = "INTERNET NET NAME";
 char* password =  "INTERNET NET PASSWORD";
 
-
-
-
 //========== GLOBALS ==========
+
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// Message buffer for MQTT messages
 char msg[100]; //length of message
+
+// Counter for messages sent
 long count=0;
 
-
-// Configuration for the DHT11 sensor
+// DHT sensor configuration
 #define DHTTYPE DHT11 // DHT 11
 int DHTPin = 2; // Pin D4 in MCU
 DHT dht(DHTPin, DHTTYPE);
 float Temperature = 0;
 float Humidity = 0;
+
+// HTTP server configuration
 WiFiServer server(80);
 
-
-
 //========== FUNCTIONS ==========
+
+// MQTT callback function
 void callback(char* topic, byte* payload, unsigned int length);
+
+// MQTT connection function
 void reconnect();
+
+// WiFi setup function
 void setup_wifi();
+
+// DHT sensor setup function
 void sensors_setup();
 
-// When start
+
+// Setup function that runs once when the board starts
 void setup() {
   Serial.begin(115200);
+
+  // Initialize DHT sensor
   sensors_setup();
+
+  // Connect to WiFi
   setup_wifi();
+
+  // Configure MQTT client
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 }
 
-// Every time
+
+// Loop function that runs continuously
 void loop() {
   
   if (!client.connected()) { //in case it loses connection
@@ -68,22 +87,28 @@ void loop() {
   }
 
   if (client.connected()){
+    // Read values from DHT sensor
     float h = dht.readHumidity(); //get values for humidity
     float t = dht.readTemperature(); //get values for temperature
 
+    // Construct message with sensor values
     String data = "Humedad: " + String(h) + "% ";
     data = data + "Temperatura: " + String(t) + "°C "; //message that will be displayed
     
+    // Print message to serial console
     Serial.println(data);
-    data.toCharArray(msg, 100);
 
+    // Convert message to char array and publish it to MQTT broker
+    data.toCharArray(msg, 100);
     client.publish(root_topic_publish, msg);
-    delay(1000) //comment line to get values in real time
+
+    // Wait for a second (optional delay for slower updating)
+    delay(1000);
   }
 
+  // Check for incoming MQTT messages
   client.loop();
 }
-
 
 
 //========= WIFI Connection =========
